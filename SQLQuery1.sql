d6b3b5a4bd29d40980fd2f6cda585a8045dd7a49
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2023-08-06 22:49:40 +0530

    SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,18 +3,18 @@ CREATE TABLE goldusers(userid integer,gold_signup_date date);
 INSERT INTO goldusers(userid,gold_signup_date) 
 VALUES (1,'09-22-2017'),
        (3,'04-21-2017'),
-	   (5,'02-04-2018'),
-	   (6,'11-10-2018'),
-	   (8,'12-12-2018'),
-	   (10,'07-01-2019');
+       (5,'02-04-2018'),
+       (6,'11-10-2018'),
+       (8,'12-12-2018'),
+       (10,'07-01-2019');
 drop table if exists users;
 CREATE TABLE users(userid integer,signup_date date); 
 INSERT INTO users(userid,signup_date) 
 VALUES (2,'09-02-2014'),
        (4,'01-15-2015'),
        (7,'04-11-2014'),
-	   (9,'05-12-2015'),
-	   (11,'02-18-2016');
+       (9,'05-12-2015'),
+       (11,'02-18-2016');
 drop table if exists orders;
 CREATE TABLE orders(userid integer,ordered_date date,item_id integer); 
 INSERT INTO orders(userid,ordered_date,item_id) 
@@ -40,18 +40,18 @@ INSERT INTO item(item_id,item_name,price)
 VALUES(1,'Aam Ras Pastry',129),
       (2,'Almonds Address Ice Cream',149),
       (3,'Bavarian Mud Ice Cream',129),
-	  (4,'Berry Cake Wonder Ice Cream',119),
-	  (5,'Big Babool Ice Cream',129),
-	  (6,'BK Chicken Burger',149),
-	  (7,'BK Veggie Burger',99),
-	  (8,'Black O Currant Milkshake',199),
-	  (9,'Butterscotch Pastry',89),
-	  (10,'Caramel Brownie Milkshake',219),
-	  (11,'Caramel Oreo Milkshake',219),
-	  (12,'Chicken Dominator Pizza',369),
-	  (13,'Chicken Whopper',199),
-	  (14,'Chickoo Caramel Milkshake',199),
-	  (15,'Makhani Do Pyaza Pizza',199);
+      (4,'Berry Cake Wonder Ice Cream',119),
+      (5,'Big Babool Ice Cream',129),
+      (6,'BK Chicken Burger',149),
+      (7,'BK Veggie Burger',99),
+      (8,'Black O Currant Milkshake',199),
+      (9,'Butterscotch Pastry',89),
+      (10,'Caramel Brownie Milkshake',219),
+      (11,'Caramel Oreo Milkshake',219),
+      (12,'Chicken Dominator Pizza',369),
+      (13,'Chicken Whopper',199),
+      (14,'Chickoo Caramel Milkshake',199),
+      (15,'Makhani Do Pyaza Pizza',199);
 
 
 --Dispaly all the created tables .
